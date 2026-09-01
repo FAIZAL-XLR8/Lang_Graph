2583791cf90a36695cb098d56ecd32ef4b72f6e7
FAIZAL-XLR8 <slif858@gmail.com>
2026-09-01 21:51:56 +0530

    my_first_workflow

--- 1_Sequential_Workflow.py
+++ 1_Sequential_Workflow.py
@@ -1,2 +1,26 @@
-from langgraph.graph import StateGraph
-nd
+from langgraph.graph import StateGraph,START,END
+from pydantic import BaseModel
+class BMI_State(BaseModel) :
+    wieght : float
+    hieght : float
+    result : float
+
+#each node is just a pythin functiojn which expects a state object and return a state object so each function should fllow this rule
+def calculate_bmi(state : BMI_State) ->BMI_State :
+    wt = state.wieght
+    ht = state.hieght
+    state.result = round(wt / (ht**2), 2)
+    return state
+graph = StateGraph(BMI_State)
+#add nodes
+graph.add_node('calculate_bmi', calculate_bmi)
+#add edges
+#start and end are dummy nodes
+graph.add_edge(START, 'calculate_bmi')
+graph.add_edge('calculate_bmi', END)
+#compile graph
+workflow = graph.compile()
+#excute graph
+intial_state = {'wieght' : 64, 'hieght' : 1.7, 'result' : 0.0}
+final_state = workflow.invoke(intial_state)
+print(final_state)
