07336badb98416b5d968458689d69300dd1b22ad
FAIZAL-XLR8 <slif858@gmail.com>
2026-09-02 12:48:04 +0530

    Added simple_LLM_worflow

--- 1_2SIMPLE_LLM_WRKFLW.py
+++ 1_2SIMPLE_LLM_WRKFLW.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph,START,END
 from pydantic import BaseModel
 load_dotenv()
 llm = HuggingFaceEndpoint(
-    repo_id='meta-llama/Llama-3.2-3B-Instruct',
-    task='text-generation'
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
 )
 chat_model = ChatHuggingFace(llm=llm)
 class LLMState(BaseModel):
@@ -26,3 +26,13 @@ def llm_qa(state : LLMState) ->LLMState :
 graph = StateGraph(LLMState)
 #add nodes
 graph.add_node('llm_qa', llm_qa)
+#add edges
+graph.add_edge(START, 'llm_qa')
+graph.add_edge('llm_qa', END)
+workflow = graph.compile()
+
+#intial_state
+initial_state = {'ques' : 'Who is Kira from Death Note','ans' : ''}
+#execute the graph
+final_state = workflow.invoke(initial_state)
+print(final_state)
